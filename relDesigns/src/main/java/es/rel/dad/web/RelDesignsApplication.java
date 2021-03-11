089d02cfa29d6c6c049ea6dc96d21bee09fd6aa2
“Rel” <jcalorsfa.rel@gmail.com>
2021-03-11 12:33:59 +0100

    Creacion del nuevo proyecto

---
+++ relDesigns/src/main/java/es/rel/dad/web/RelDesignsApplication.java
@@ -0,0 +1,13 @@
+package es.rel.dad.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelDesignsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelDesignsApplication.class, args);
+	}
+
+}
