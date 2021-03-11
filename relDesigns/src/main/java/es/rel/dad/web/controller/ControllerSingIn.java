fbcf3f0518e7e24dd6c21b7524d03ec306272638
“Rel” <jcalorsfa.rel@gmail.com>
2021-03-12 00:12:39 +0100

    Correcion del controller SingIn

--- relDesigns/src/main/java/es/rel/dad/web/controller/ControllerSingIn.java
+++ relDesigns/src/main/java/es/rel/dad/web/controller/ControllerSingIn.java
@@ -1,6 +1,7 @@
 package es.rel.dad.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import es.rel.dad.web.entity.Client;
 import es.rel.dad.web.repository.ClientRepository;
 
+@Controller
 public class ControllerSingIn {
 
 	@Autowired private ClientRepository client;
