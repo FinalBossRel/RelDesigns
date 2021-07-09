7668b41366ff76c290902e29df70ad5a358aee4b
“Rel” <jcalorsfa.rel@gmail.com>
2021-07-09 05:08:21 +0200

    Revert "Arreglo de errores"
    This reverts commit 863911c91e0c41ca5ac0b4ed9cf12a6365e8dff1.

--- relDesigns/src/main/java/es/rel/dad/web/controller/ControllerSingIn.java
+++ relDesigns/src/main/java/es/rel/dad/web/controller/ControllerSingIn.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,30 +19,29 @@ import es.rel.dad.web.repository.ClientRepository;
 public class ControllerSingIn {
 
 	@Autowired private ClientRepository client;
-	@Autowired private PasswordEncoder passwordEncoder;
 
 	@PostMapping("/newUser")
-	public String newUser(Model model, @RequestParam String name, @RequestParam String surname, @RequestParam String telephone, @RequestParam String mail, @RequestParam String address, @RequestParam String password, HttpServletRequest request) 
+	public String newUser(Model model, @RequestParam String name, @RequestParam String surname, @RequestParam String telephone, @RequestParam String mail, @RequestParam String address, @RequestParam String password) 
 	{
-
 		
-		Client c = new Client(name, surname,Integer.parseInt(telephone), mail, address, passwordEncoder.encode(password), "USER");
+		Client c = new Client(name, surname,Integer.parseInt(telephone), mail, address, password);
 		client.save(c);
-
+		model.addAttribute("id",c.getId());
+		model.addAttribute("client", c);
 		return "home";
 	}
 	
-	/*
+/*	
 	  @PostMapping("/loginBBDD") 
 	  public String login(Model model, @RequestParam String name, @RequestParam String password,  HttpServletRequest request) { 
 		  Client c = client.findByNameAndPassword(name,password); 
 		  if(c !=null) {
 			  model.addAttribute("fallo", false);
 			  model.addAttribute("id",c.getId()); model.addAttribute("client", c); } 
-		  return "home";
+		  return "home"; 
 	  }
-	 */
-
+	 
+*/
 	@GetMapping("/datosClient/{name}")
 	public String datosClient(Model model, @PathVariable String name,  HttpServletRequest request) {
 		Optional<Client> c = client.findByName(name);
@@ -55,9 +53,8 @@ public class ControllerSingIn {
 
 		return "datosClient";
 	}
-	
-	@GetMapping("/nuevoUsuario")
-	public String createaccount(Model model,  HttpServletRequest request) {
+	@GetMapping("/createaccount")
+	public String createaccount(Model model) {
 		return "createaccount";
 	}
 	
